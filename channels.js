d92805f39681dab2cb8c21c6e078afe796f145ed
Kaj Rumpff <rumpff@proton.me>
2026-02-06 15:12:26 +0100

    data driven channel generation

---
+++ channels.js
@@ -0,0 +1,27 @@
+const channelLayout = ["disc"];
+
+const channelLibrary = {
+    "empty": {   
+        iconLayout:`
+            <div class="mii-grid-preview">
+                <div class="mii-head"></div>
+                <div class="mii-head"></div>
+            </div>`,
+        bannerLayout: ``,
+        playURL: ""
+    },
+    
+    "disc": {
+        iconLayout:`
+            <div class="mii-grid-preview">
+                <div class="mii-head"></div>
+                <div class="mii-head"></div>
+            </div>`,
+        bannerLayout:`
+            <div class="mii-grid-preview">
+                <div class="mii-head"></div>
+                <div class="mii-head"></div>
+            </div>`,
+        playURL: ""
+    }
+}
--- main.js
+++ main.js
@@ -1,13 +1,58 @@
-const overlay = document.querySelector('.channel-overlay');
-const channels = document.querySelectorAll('.channel');
+const menuTrack = document.querySelector('.menu-track');
+const channelsPerPage = 12;
 
-channels.forEach(channel => {
-    channel.addEventListener("click", openChannel);
-});
+function initMenu() {
+    createIcons();
+    createBanners();
+}
+
+function createIcons() {
+
+    let channelCount;
+    let currentPage;
+
+    for(channelCount = 0; channelCount < channelLayout.length; channelCount++) {
+
+        // create new page
+        if(channelCount === 0 || channelCount % channelsPerPage === 0) {
+            currentPage = document.createElement('section');
+            currentPage.className = "menu-page"
+            menuTrack.appendChild(currentPage);
+        }
+
+        createIcon(channelLayout[channelCount], currentPage)
+    }
+
+    let remainingChannelSlots = channelsPerPage - (channelCount % channelsPerPage);
+    for(let i = 0; i < remainingChannelSlots; i++){
+        createIcon("empty", currentPage);
+    }
+}
+
+function createIcon(channelId, menuPage) {
+    let icon = document.createElement('section');
+    menuPage.appendChild(icon);
+
+    icon.className = "channel-icon"
+    icon.dataset.id = channelId;
+
+    if(channelLibrary[channelId].bannerLayout != ``)
+    {
+        icon.addEventListener("click", openChannel(channelId));
+    }
+}
+
+function createBanners() {
+
+}
+
+initMenu();
+
+const overlay = document.querySelector('.channel-banner');
 
 overlay.addEventListener("click", closeChannel);
 
-function openChannel() {
+function openChannel(channelId) {
     overlay.dataset.state = "visible";
 }
 
