ac94dcaf2b40cc9de8fb1bb1b48a03b51970b252
Kaj Rumpff <rumpff@proton.me>
2026-02-14 20:51:02 +0100

    add cursor animation

--- src/cursor.js
+++ src/cursor.js
@@ -1,30 +1,60 @@
-const cursorBaseScale = 0.46;
-const shadowOffset = 5;
+let cursor;
+let cursorShadow;
+
+const baseCursorScale = 0.46;
+const baseShadowOffset = 5;
+const baseCursorAngle = -6;
+
+const cursorAnimationSensitivity = 1.3;
+
 let cursorAngle = -3;
 
-document.body.style.cursor = 'none';
+let lastMove = performance.now();
+let lastX = 0;
 
-const cursorShadow = document.createElement('img');
-cursorShadow.className = 'cursor-shadow';
-cursorShadow.src = 'assets/cursor-shadow.png';
-document.body.appendChild(cursorShadow);
+let idleTimer;
+const idleThreshold = 2;
 
-const cursor = document.createElement('img');
-cursor.className = 'cursor';
-cursor.src = 'assets/cursor.png';
-document.body.appendChild(cursor);
+initCursor();
 
 
-updateCursorTransforms(0,0)
+function initCursor() {
+  document.body.style.cursor = 'none';
 
-window.addEventListener('mousemove', (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
-  
-  updateCursorTransforms(x, y)
-});
+  cursorShadow = document.createElement('img');
+  cursorShadow.className = 'cursor-shadow';
+  cursorShadow.src = 'assets/cursor-shadow.png';
+  document.body.appendChild(cursorShadow);
 
-function updateCursorTransforms(x, y) {
-  cursor.style.transform = `translate3d(${x}px, ${y}px, 0) translate(-50%, -50%) rotate(${cursorAngle}deg) scale(calc(var(--channel-scale) * ${cursorBaseScale}))`;
-  cursorShadow.style.transform = `translate3d(${x + shadowOffset}px, ${y + shadowOffset}px, 0) translate(-50%, -50%) rotate(${cursorAngle}deg) scale(calc(var(--channel-scale) * ${cursorBaseScale}))`;
+  cursor = document.createElement('img');
+  cursor.className = 'cursor';
+  cursor.src = 'assets/cursor.png';
+  document.body.appendChild(cursor);
+
+
+  updateCursorTransforms(0,0);
+
+  window.addEventListener('mousemove', (e) => {   
+    updateCursorTransforms(e.clientX, e.clientY);
+    lastMove = performance.now(); 
+    lastX = e.clientX;
+
+    clearTimeout(idleTimer);
+    idleTimer = setTimeout(() => { cursorAngle = baseCursorAngle; updateCursorTransforms(e.clientX, e.clientY, true) }, idleThreshold);
+  });
+}
+
+
+
+function updateCursorTransforms(x, y, skipAngle = false) {
+  // calculate cursor angle
+  if(!skipAngle) {
+    cursorAngle = baseCursorAngle + (-(lastX - x) / channelScale * cursorAnimationSensitivity);
+  }
+
+  cursor.style.setProperty('--angle', cursorAngle + 'deg');
+  cursorShadow.style.setProperty('--angle', cursorAngle  + 'deg');
+
+  cursor.style.transform = `translate3d(${x}px, ${y}px, 0) translate(-50%, -50%) rotate(var(--angle)) scale(calc(var(--channel-scale) * ${baseCursorScale}))`;
+  cursorShadow.style.transform = `translate3d(calc(${x}px + (${baseShadowOffset}px * var(--channel-scale))), calc(${y}px + (${baseShadowOffset}px * var(--channel-scale))), 0) translate(-50%, -50%) rotate(var(--angle)) scale(calc(var(--channel-scale) * ${baseCursorScale}))`;
 }
